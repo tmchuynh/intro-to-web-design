cb906f3f5f422147eeb96e082744ef12d76c56d7
Tina Huynh <tinamchuynh@gmail.com>
2025-07-05 17:37:56 -0700

    feat: restructure fallback navigation to enhance clarity and organization of web design topics

--- src/utils/fallback.ts
+++ src/utils/fallback.ts
@@ -1,403 +1,128 @@
 export const fallbackNav = [
   {
-    title: "Fundamentals",
+    title: "Home",
     items: [
       {
-        title: "Overview",
+        title: "Introduction to Web Design",
         href: "/",
       },
+    ],
+  },
+  {
+    title: "Web Design Fundamentals",
+    items: [
       {
-        title: "Getting Started",
-        href: "/getting-started",
-        children: [
-          {
-            title: "Need To Knows",
-            href: "/getting-started/need-to-knows",
-          },
-          {
-            title: "Best Coding Practices",
-            href: "/getting-started/best-coding-practices",
-          },
-          {
-            title: "Accessibility Fundamentals",
-            href: "/getting-started/accessibility-fundamentals",
-          },
-        ],
+        title: "Introduction to Web Design",
+        href: "/introduction-to-web-design/introduction-to-web-design",
       },
       {
-        title: "Vocabulary",
-        href: "/vocabulary",
+        title: "User Experience Basics",
+        href: "/introduction-to-web-design/user-experience-basics",
       },
       {
-        title: "Abbreviations",
-        href: "/abbreviations",
+        title: "User Interface Design",
+        href: "/introduction-to-web-design/user-interface-design",
       },
     ],
   },
   {
-    title: "Developer Tools & Resources",
+    title: "Design Fundamentals",
     items: [
       {
-        title: "Developer Tools & Resources",
-        href: "/developer-tools-and-resources",
-        children: [
-          {
-            title: "Browser Developer Tools",
-            href: "/developer-tools-and-resources/browser-developer-tools",
-          },
-          {
-            title: "Command Line Interface",
-            href: "/developer-tools-and-resources/command-line-interface",
-          },
-          {
-            title: "Git and Github",
-            href: "/developer-tools-and-resources/git-and-github",
-          },
-          {
-            title: "Hosting and Deployment",
-            href: "/developer-tools-and-resources/hosting-and-deployment",
-          },
-          {
-            title: "Package Managers",
-            href: "/developer-tools-and-resources/package-managers",
-          },
-        ],
+        title: "Color Theory",
+        href: "/design-fundamentals/color-theory",
+      },
+      {
+        title: "Design Patterns",
+        href: "/design-fundamentals/design-patterns",
+      },
+      {
+        title: "Design Principles",
+        href: "/design-fundamentals/design-principles",
+      },
+      {
+        title: "Interactivity",
+        href: "/design-fundamentals/interactivity-fundamentals",
+      },
+      {
+        title: "Typography",
+        href: "/design-fundamentals/typography",
       },
     ],
   },
   {
-    title: "Core Technologies",
+    title: "Branding Fundamentals",
     items: [
       {
-        title: "SEO & Accessibility",
-        href: "/seo-accessibility",
+        title: "Branding Fundamentals",
+        href: "/branding-fundamentals/branding-fundamentals",
       },
       {
-        title: "HTML",
-        href: "/introduction-to-html",
-        children: [
-          {
-            title: "Overview",
-            href: "/introduction-to-html/overview",
-          },
-          {
-            title: "Semantic HTML",
-            href: "/introduction-to-html/semantic-html",
-          },
-          {
-            title: "HTML Templates",
-            href: "/introduction-to-html/html-templates",
-          },
-          {
-            title: "Element Hierarchy",
-            href: "/introduction-to-html/element-hierarchy",
-          },
-          {
-            title: "Elements and Attributes",
-            href: "/introduction-to-html/elements-and-attributes",
-          },
-          {
-            title: "Style Elements",
-            href: "/introduction-to-html/style-elements",
-          },
-        ],
+        title: "Case Studies",
+        href: "/branding-fundamentals/case-studies",
       },
       {
-        title: "CSS",
-        href: "/introduction-to-css",
-        children: [
-          {
-            title: "Selectors",
-            href: "/introduction-to-css/selectors",
-          },
-          {
-            title: "Responsive Design",
-            href: "/introduction-to-css/responsive-design",
-          },
-          {
-            title: "Frameworks",
-            href: "/introduction-to-css/frameworks",
-          },
-          {
-            title: "Debugging",
-            href: "/introduction-to-css/debugging",
-          },
-        ],
+        title: "Design Tools",
+        href: "/branding-fundamentals/design-tools",
       },
       {
-        title: "JavaScript",
-        href: "/introduction-to-javascript",
-        children: [
-          {
-            title: "Data Types",
-            href: "/introduction-to-javascript/data-types",
-          },
-          {
-            title: "Functions Objects",
-            href: "/introduction-to-javascript/functions-objects",
-          },
-          {
-            title: "ES6 Features",
-            href: "/introduction-to-javascript/es6-features",
-          },
-          {
-            title: "Browser APIs",
-            href: "/introduction-to-javascript/browser-apis",
-          },
-        ],
-      },
-      {
-        title: "Document Object Model",
-        href: "/document-object-model",
-        children: [
-          {
-            title: "Manipulation",
-            href: "/document-object-model/manipulation",
-          },
-          {
-            title: "Events",
-            href: "/document-object-model/events",
-          },
-        ],
-      },
-      {
-        title: "Forms",
-        href: "/forms",
-      },
-      {
-        title: "jQuery",
-        href: "/jQuery",
-      },
-      {
-        title: "Documentation and Learning Platforms",
-        href: "/documentation-and-learning-platforms",
+        title: "Visual Design Principles",
+        href: "/branding-fundamentals/visual-design-principles",
       },
     ],
   },
   {
-    title: "Design",
+    title: "Design Systems Fundamentals",
     items: [
       {
-        title: "UX/UI Design",
-        href: "/UX-UI-Design",
-        children: [
-          {
-            title: "Wireframing",
-            href: "/UX-UI-Design/wireframing",
-          },
-          {
-            title: "Design Trends",
-            href: "/UX-UI-Design/design-trends",
-          },
-          {
-            title: "Typography",
-            href: "/UX-UI-Design/typography",
-          },
-        ],
+        title: "Design Systems",
+        href: "/design-systems-fundamentals/design-systems",
+      },
+      {
+        title: "Design Tokens",
+        href: "/design-systems-fundamentals/design-tokens",
+      },
+      {
+        title: "Style Guides",
+        href: "/design-systems-fundamentals/style-guides",
       },
     ],
   },
   {
-    title: "Projects",
+    title: "Technical Skills",
     items: [
       {
-        title: "Quiz App",
-        href: "/quiz-app",
-        children: [
-          {
-            title: "Part 1",
-            href: "/quiz-app/part-1",
-          },
-          {
-            title: "Part 2",
-            href: "/quiz-app/part-2",
-          },
-          {
-            title: "Part 3",
-            href: "/quiz-app/part-3",
-          },
-        ],
+        title: "CSS Frameworks",
+        href: "/technicals-skills/CSS-frameworks",
       },
       {
-        title: "Website Portfolio",
-        href: "/website-portfolio",
-        children: [
-          {
-            title: "Part 1",
-            href: "/website-portfolio/part-1",
-          },
-          {
-            title: "Part 2",
-            href: "/website-portfolio/part-2",
-          },
-        ],
+        title: "HTML and CSS Basics",
+        href: "/technicals-skills/HTML-and-CSS-basics",
+      },
+      {
+        title: "HTTP Protocol",
+        href: "/technicals-skills/HTTP-protocol",
+      },
+      {
+        title: "Responsive and Adaptive Design",
+        href: "/technicals-skills/responsive-and-adaptive-design",
+      },
+      {
+        title: "Version Control",
+        href: "/technicals-skills/version-control",
+      },
+      {
+        title: "Web Performance",
+        href: "/technicals-skills/web-performance",
       },
     ],
   },
   {
-    title: "Utilities & Tools",
+    title: "Practical Tools",
     items: [
       {
-        title: "Resources & Utilities",
-        href: "/resources-utilities",
-      },
-      {
-        title: "Helper Functions",
-        href: "/helper-functions",
-      },
-      {
-        title: "Development Tools",
-        href: "/development-tools",
-      },
-      {
-        title: "Utility Libraries",
-        href: "/utility-libraries",
-      },
-    ],
-  },
-  {
-    title: "Advanced Topics",
-    items: [
-      {
-        title: "Performance",
-        href: "/performance",
-      },
-      {
-        title: "Security",
-        href: "/security",
-      },
-      {
-        title: "Introduction To APIs",
-        href: "/introduction-to-APIs",
-        children: [
-          {
-            title: "Overview",
-            href: "/introduction-to-APIs/overview",
-          },
-          {
-            title: "API Technologies",
-            href: "/introduction-to-APIs/API-technologies",
-            children: [
-              {
-                title: "API Gateways",
-                href: "/introduction-to-APIs/API-technologies/API-gateways",
-              },
-              {
-                title: "REST APIs",
-                href: "/introduction-to-APIs/API-technologies/REST-APIs",
-              },
-              {
-                title: "GraphQL",
-                href: "/introduction-to-APIs/API-technologies/GraphQL",
-              },
-            ],
-          },
-          {
-            title: "API Design",
-            href: "/introduction-to-APIs/API-design",
-          },
-          {
-            title: "API Security",
-            href: "/introduction-to-APIs/API-security",
-          },
-        ],
-      },
-      {
-        title: "Frameworks",
-        href: "/frameworks",
-      },
-      {
-        title: "Libraries",
-        href: "/libraries",
-      },
-      {
-        title: "TypeScript",
-        href: "/typescript",
-      },
-      {
-        title: "React",
-        href: "/react",
-        children: [
-          {
-            title: "Overview",
-            href: "/react/overview",
-          },
-          {
-            title: "Next.js Overview",
-            href: "/react/nextjs/overview",
-          },
-          {
-            title: "Next.js Components",
-            href: "/react/nextjs/components",
-          },
-          {
-            title: "Next.js Routing",
-            href: "/react/nextjs/routing",
-          },
-          {
-            title: "Remix",
-            href: "/react/remix",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    title: "Advanced Projects",
-    items: [
-      {
-        title: "Blog Website",
-        href: "/blog-website",
-        children: [
-          {
-            title: "Setup",
-            href: "/blog-website/setup",
-          },
-          {
-            title: "Content Management",
-            href: "/blog-website/content-management",
-          },
-          {
-            title: "Deployment",
-            href: "/blog-website/deployment",
-          },
-        ],
-      },
-      {
-        title: "E-commerce Platform",
-        href: "/ecommerce-platform",
-        children: [
-          {
-            title: "Product Catalog",
-            href: "/ecommerce-platform/product-catalog",
-          },
-          {
-            title: "Shopping Cart",
-            href: "/ecommerce-platform/shopping-cart",
-          },
-          {
-            title: "Payment Integration",
-            href: "/ecommerce-platform/payment-integration",
-          },
-        ],
-      },
-      {
-        title: "Advancing Your Skills",
-        href: "/advancing-your-skills",
-        children: [
-          {
-            title: "Career Paths",
-            href: "/advancing-your-skills/career-paths",
-          },
-          {
-            title: "Continuous Learning",
-            href: "/advancing-your-skills/continuous-learning",
-          },
-          {
-            title: "Portfolio Development",
-            href: "/advancing-your-skills/portfolio-development",
-          },
-        ],
+        title: "Practical Tools",
+        href: "/practical-tools",
       },
     ],
   },
