908c70e769142ed13872a37d83f923f5b4e811fa
Tina Huynh <tinamchuynh@gmail.com>
2025-07-05 19:33:28 -0700

    feat: refactor Guides and Resources components to accept dynamic props and implement card layout

--- src/components/Supplementary.tsx
+++ src/components/Supplementary.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { FaChevronRight } from "react-icons/fa";
 import { Card } from "./ui/card";
 
-export default function Resources({
-  title = "Additional Resources",
+export default function Supplementary({
+  title = "Supplementary Reading",
   description = "Explore these resources to deepen your understanding of web design and development. These links provide valuable insights, tutorials, and documentation to help you enhance your skills. Whether you're looking for design inspiration, technical documentation, or best practices, these resources will guide you on your journey. Make the most of these curated links to expand your knowledge and stay updated with the latest trends in web design. Happy learning!",
   resources,
 }: {
@@ -24,16 +24,18 @@ export default function Resources({
               href={resource.href}
               className="group relative no-underline"
             >
-              <Card className="relative gap-2 shadow p-6 border rounded-xl h-full w-full">
-                <div className="inline-flex items-center space-x-3">
-                  <h3 className="font-medium text-lg underline-offset-3 group-hover:underline decoration-1">
-                    {resource.title}
-                  </h3>
+              <Card className="relative flex flex-col gap-2 justify-between shadow p-6 border rounded-xl h-full w-full">
+                <div>
+                  <div className="inline-flex items-start space-x-3 w-4/5">
+                    <h3 className="font-medium text-lg underline-offset-3 group-hover:underline decoration-1">
+                      {resource.title}
+                    </h3>
+                  </div>
+                  <p className="text-sm">{resource.description}</p>
                 </div>
-                <p className="text-sm">{resource.description}</p>
-                <p className="my-0 font-medium text-sm">
-                  View resource
-                  <FaChevronRight className="inline ml-2 duration-300 group-hover:translate-x-2 ease-in-out" />
+                <p className="flex items-center mt-2 text-sm">
+                  Read more{" "}
+                  <FaChevronRight className="inline ml-1 duration-300 group-hover:translate-x-2 ease-in-out" />
                 </p>
               </Card>
             </Link>
