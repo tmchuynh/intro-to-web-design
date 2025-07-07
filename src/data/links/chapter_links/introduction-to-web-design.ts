0cf1f86f97121c51af370460881bafae984d4d39
Tina Huynh <tinamchuynh@gmail.com>
2025-07-06 19:59:38 -0700

    feat: add resources, articles, and courses to web design documentation

---
+++ src/data/links/chapter_links/introduction-to-web-design.ts
@@ -0,0 +1,269 @@
+import { resourceLink } from "@/lib/interface";
+
+export const articles: resourceLink[] = [
+  {
+    title: "The Principles of Beautiful Web Design",
+    href: "https://www.smashingmagazine.com/2008/01/10-principles-of-effective-web-design/",
+    description: "A comprehensive guide to fundamental web design principles",
+  },
+  {
+    title: "Web Design 101: A Complete Guide for Digital Designers",
+    href: "https://webflow.com/blog/web-design-101",
+    description: "Essential concepts and practices for aspiring web designers",
+  },
+  {
+    title: "The Elements of User Experience",
+    href: "https://www.nngroup.com/articles/elements-user-experience/",
+    description:
+      "Understanding the key elements that create meaningful user experiences",
+  },
+  {
+    title: "Color Theory in Web Design",
+    href: "https://www.smashingmagazine.com/2010/02/color-theory-for-designers-part-1-the-meaning-of-color/",
+    description:
+      "Understanding color psychology and its application in web design",
+  },
+  {
+    title: "Responsive Design Best Practices",
+    href: "https://medium.com/better-programming/responsive-design-best-practices-c6d3f5f98378",
+    description: "Modern approaches to creating responsive websites",
+  },
+  {
+    title: "Typography in Web Design",
+    href: "https://www.smashingmagazine.com/2018/06/digital-typography-guide/",
+    description: "Complete guide to typography for web designers",
+  },
+  {
+    title: "Grid Systems in Web Design",
+    href: "https://www.creativebloq.com/web-design/grid-theory-41411345",
+    description: "Understanding and implementing grid systems effectively",
+  },
+  {
+    title: "Web Accessibility Fundamentals",
+    href: "https://www.w3.org/WAI/fundamentals/accessibility-intro/",
+    description: "Essential guide to making websites accessible to all users",
+  },
+  {
+    title: "UX Design Principles",
+    href: "https://www.interaction-design.org/literature/topics/ux-design",
+    description: "Core principles of user experience design",
+  },
+  {
+    title: "Mobile-First Design Approach",
+    href: "https://www.lukew.com/ff/entry.asp?933",
+    description:
+      "Understanding and implementing mobile-first design strategies",
+  },
+  {
+    title: "Web Design Psychology",
+    href: "https://www.smashingmagazine.com/2011/03/how-to-use-psychology-to-design-effective-landing-pages/",
+    description:
+      "Psychological principles in web design and their impact on user behavior",
+  },
+  {
+    title: "Visual Hierarchy in Web Design",
+    href: "https://www.webdesignerdepot.com/2015/02/the-ultimate-guide-to-visual-hierarchy/",
+    description:
+      "Creating effective visual hierarchies for better user experience",
+  },
+];
+
+export const courses: resourceLink[] = [
+  {
+    title: "Web Design for Everybody: Basics of Web Development & Coding",
+    href: "https://www.coursera.org/specializations/web-design",
+    description:
+      "University of Michigan's comprehensive web design course on Coursera",
+  },
+  {
+    title: "FreeCodeCamp - Responsive Web Design",
+    href: "https://www.freecodecamp.org/learn/responsive-web-design/",
+    description:
+      "Free, interactive course covering HTML, CSS, and responsive design principles",
+  },
+  {
+    title: "UI/UX Design Specialization",
+    href: "https://www.coursera.org/specializations/ui-ux-design",
+    description:
+      "Complete course on user interface and experience design fundamentals",
+  },
+  {
+    title: "The Complete Web Developer in 2025",
+    href: "https://www.udemy.com/course/the-complete-web-developer-zero-to-mastery/",
+    description:
+      "Comprehensive web development bootcamp from zero to professional",
+  },
+  {
+    title: "Design Thinking Specialization",
+    href: "https://www.coursera.org/specializations/design-thinking-innovation",
+    description: "Learn design thinking methodology for web and product design",
+  },
+  {
+    title: "Frontend Web Developer Nanodegree",
+    href: "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011",
+    description: "Professional certification program in frontend development",
+  },
+  {
+    title: "CSS for JavaScript Developers",
+    href: "https://css-for-js.dev/",
+    description: "Advanced CSS course tailored for JavaScript developers",
+  },
+  {
+    title: "Web Design & Development Track",
+    href: "https://teamtreehouse.com/tracks/web-design",
+    description: "Comprehensive web design and development learning path",
+  },
+  {
+    title: "Advanced CSS and Sass",
+    href: "https://www.udemy.com/course/advanced-css-and-sass/",
+    description: "Master modern CSS techniques and Sass",
+  },
+  {
+    title: "Design Systems Course",
+    href: "https://designcode.io/design-system-in-figma",
+    description: "Learn to create and implement design systems",
+  },
+  {
+    title: "Web Accessibility by Google",
+    href: "https://www.udacity.com/course/web-accessibility--ud891",
+    description: "Learn to create accessible web experiences",
+  },
+  {
+    title: "React for Designers",
+    href: "https://designcode.io/react",
+    description: "Learn React development from a designer's perspective",
+  },
+];
+
+export const resources: resourceLink[] = [
+  {
+    title: "MDN Web Docs",
+    href: "https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web",
+    description:
+      "Mozilla's comprehensive guide to web development fundamentals",
+  },
+  {
+    title: "Google Web Fundamentals",
+    href: "https://developers.google.com/web/fundamentals",
+    description: "Google's resource for modern web development best practices",
+  },
+  {
+    title: "W3Schools",
+    href: "https://www.w3schools.com/",
+    description: "Interactive tutorials and references for web technologies",
+  },
+  {
+    title: "Adobe Color",
+    href: "https://color.adobe.com/create/color-wheel",
+    description: "Color wheel and palette generation tool for designers",
+  },
+  {
+    title: "Figma",
+    href: "https://www.figma.com/",
+    description:
+      "Collaborative interface design tool with web-focused features",
+  },
+  {
+    title: "CodePen",
+    href: "https://codepen.io/",
+    description: "Online code editor and social development environment",
+  },
+  {
+    title: "Can I Use",
+    href: "https://caniuse.com/",
+    description: "Browser support tables for modern web technologies",
+  },
+  {
+    title: "Web.dev",
+    href: "https://web.dev/",
+    description: "Guidance for modern web development by Google",
+  },
+  {
+    title: "CSS-Tricks",
+    href: "https://css-tricks.com/",
+    description: "Tips, tricks, and techniques for web designers",
+  },
+  {
+    title: "Frontend Mentor",
+    href: "https://www.frontendmentor.io/",
+    description: "Real-world practice projects for web designers",
+  },
+  {
+    title: "Coolors",
+    href: "https://coolors.co/",
+    description: "Fast color scheme generator for designers",
+  },
+  {
+    title: "Font Pair",
+    href: "https://fontpair.co/",
+    description: "Typography pairing suggestions for web design",
+  },
+  {
+    title: "DevDocs",
+    href: "https://devdocs.io/",
+    description: "Comprehensive API documentation browser",
+  },
+];
+
+export const books: resourceLink[] = [
+  {
+    title: "Don't Make Me Think",
+    href: "https://www.amazon.com/Dont-Make-Think-Revisited-Usability/dp/0321965515",
+    description: "Steve Krug's essential guide to web usability",
+  },
+  {
+    title: "HTML and CSS: Design and Build Websites",
+    href: "https://www.amazon.com/HTML-CSS-Design-Build-Websites/dp/1118008189",
+    description: "Jon Duckett's beautifully designed guide to HTML and CSS",
+  },
+  {
+    title: "Atomic Design",
+    href: "https://atomicdesign.bradfrost.com/",
+    description: "Brad Frost's comprehensive guide to design systems",
+  },
+  {
+    title: "Responsive Web Design",
+    href: "https://www.amazon.com/Responsive-Design-Brief-People-Websites/dp/098444257X",
+    description: "Ethan Marcotte's seminal book on responsive design",
+  },
+  {
+    title: "Design for Hackers",
+    href: "https://www.amazon.com/Design-Hackers-Reverse-Engineering-Beauty/dp/1119998956",
+    description: "David Kadavy's guide to design principles for developers",
+  },
+  {
+    title: "100 Ideas that Changed Web Design",
+    href: "https://www.amazon.com/100-Ideas-that-Changed-Design/dp/1786275066",
+    description: "Steven Heller's exploration of web design evolution",
+  },
+  {
+    title: "Designing with Web Standards",
+    href: "https://www.amazon.com/Designing-Web-Standards-Jeffrey-Zeldman/dp/0321616952",
+    description: "Jeffrey Zeldman's guide to web standards and best practices",
+  },
+  {
+    title: "The Elements of User Experience",
+    href: "https://www.amazon.com/Elements-User-Experience-User-Centered-Design/dp/0321683684",
+    description: "Jesse James Garrett's framework for user experience design",
+  },
+  {
+    title: "Web Design with HTML, CSS, JavaScript and jQuery Set",
+    href: "https://www.amazon.com/Web-Design-HTML-JavaScript-jQuery/dp/1118907442",
+    description: "Jon Duckett's comprehensive web development book set",
+  },
+  {
+    title: "Practical Web Design",
+    href: "https://www.amazon.com/Practical-Web-Design-Learn-start/dp/1788395573",
+    description: "Philippe Hong's practical approach to modern web design",
+  },
+  {
+    title: "Learning Web Design",
+    href: "https://www.amazon.com/Learning-Web-Design-Beginners-JavaScript/dp/1491960205",
+    description: "Jennifer Robbins' comprehensive guide for beginners",
+  },
+  {
+    title: "Inclusive Design Patterns",
+    href: "https://www.smashingmagazine.com/printed-books/inclusive-front-end-design-patterns/",
+    description: "Heydon Pickering's guide to accessible web design",
+  },
+];
